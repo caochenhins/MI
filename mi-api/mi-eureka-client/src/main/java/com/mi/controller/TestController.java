dc8e4c3df032c1594e241f2f43a7004fed5239b5
yesh <454514080@qq.com>
2017-09-13 19:20:00 +0800

    Swagger2 简易高可用案例

--- mi-api/mi-eureka-client/src/main/java/com/mi/controller/TestController.java
+++ mi-api/mi-eureka-client/src/main/java/com/mi/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.mi.controller;
 
 import com.mi.common.annotation.Pro;
-import io.swagger.annotations.*;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Swagger 控制器案例模版
+ *
  * @author yesh
  *         (M.M)!
  *         Created by 2017/5/7.
@@ -17,15 +23,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/swagger")
 public class TestController {
 
-    @ApiOperation(value = "TEST接口简介",notes = "接口详细描述")
-    @ApiImplicitParam(name = "user",dataType = "User")
-    @RequestMapping(value = "helloTest" ,method = RequestMethod.POST)
-    public String hello(@RequestBody User user){
-        return "Hello Swagger : my name is "+user.getName();
+    @ApiOperation(value = "TEST接口简介", notes = "接口详细描述")
+    @RequestMapping(value = "helloTest", method = RequestMethod.POST)
+    public UserRes hello(@RequestBody User user) {
+        UserRes u = new UserRes();
+        u.setId(user.getId());
+        return u;
     }
 }
-class  User {
+
+@ApiModel(value = "User//参数实体")
+class User {
+
+    @ApiModelProperty(value = "主键")
     private int id;
+    @ApiModelProperty(value = "姓名")
     private int name;
 
     public int getId() {
@@ -43,4 +55,30 @@ class  User {
     public void setName(int name) {
         this.name = name;
     }
+}
+
+@ApiModel(value = "UserRes//返回实体")
+class UserRes {
+
+    @ApiModelProperty(value = "返回主键")
+    private int id;
+    @ApiModelProperty(value = "返回姓名")
+    private int name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
 }
